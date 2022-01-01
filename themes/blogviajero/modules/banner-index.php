a6a2316090f210f68526ed1e64eee78962e4d271
lea.arturi <lea.arturi@gmail.com>
2022-01-01 13:11:02 -0300

    Modularizo html a php

---
+++ themes/blogviajero/modules/banner-index.php
@@ -0,0 +1,53 @@
+<!--=====================================
+BANNER
+======================================-->
+<div class="bannerEstatico"></div>
+
+<section class="jd-slider fade-slider">
+
+    <div class="slide-inner">
+
+        <ul class="slide-area">
+
+            <li>
+
+                <img src="img/banner01.jpg" alt="Juanito Travel en el mar" class="img-fluid">
+
+            </li>
+
+            <li>
+
+                <img src="img/banner02.jpg" alt="Juanito Travel en el mar" class="img-fluid">
+
+            </li>
+
+            <li>
+
+                <img src="img/banner03.jpg" alt="Juanito Travel en el mar" class="img-fluid">
+
+            </li>
+
+            <li>
+
+                <img src="img/banner04.jpg" alt="Juanito Travel en el mar" class="img-fluid">
+
+            </li>
+
+        </ul>
+
+    </div>
+
+    <div class="controller d-none">
+
+        <a class="auto" href="#">
+
+            <i class="fas fa-play fa-xs"></i>
+            <i class="fas fa-pause fa-xs"></i>
+
+        </a>
+
+        <div class="indicate-area"></div>
+
+    </div>
+
+</section>
